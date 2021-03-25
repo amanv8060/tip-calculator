4e5af032574e8d158973abdedba79df86ad2f887
Aman Verma <verma1090aman@gmail.com>
2021-03-25 21:54:32 +0530

    Starter Template

---
+++ lib/tipCalculator.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class TipCalculator extends StatefulWidget {
+  TipCalculator({Key key}) : super(key: key);
+
+  @override
+  _TipCalculatorState createState() => _TipCalculatorState();
+}
+
+class _TipCalculatorState extends State<TipCalculator> {
+
+
+@override
+Widget build(BuildContext context) {
+  return Container();
+}
+}
